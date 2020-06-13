091c04379763c2bae385e4ed7fc404301a65ee62
wd9746 <wd974655958@163.com>
2020-06-13 17:26:36 +0800

    Wd-First revision

---
+++ src/main/java/com/dt/springcloud/Wd.java
@@ -0,0 +1,13 @@
+package com.dt.springcloud;
+
+/**
+ * @ProjectName: test-cloud
+ * @ClassName: Wd
+ * @Description: TODO(一句话描述该类的功能)
+ * @Author: WD
+ * @Date: 2020/6/13 17:26
+ */
+public class Wd {
+
+    private int id;
+}
